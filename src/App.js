20afb08e9cf484d35eb3e4a2dc04a11b3161c78f
mbugua <mbuguafaimba@gmail.com>
2023-03-23 15:27:01 +0300

    added a route for the signup page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </Router>
