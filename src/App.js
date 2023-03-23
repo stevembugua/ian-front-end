5e4c631809cf3205c408a04ffb6da8c0c513cac7
mbugua <mbuguafaimba@gmail.com>
2023-03-23 15:33:59 +0300

    added route for the todolist component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import TodoList from './components/TodoList';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/todo" element={<TodoList />} />
         </Routes>
       </div>
     </Router>
